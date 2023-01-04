ad5dfbabddcdfec6a839b5601a9b0270be0c36b8
unknown <vikasbajantri@gmail.com>
2023-01-04 14:33:38 +0530

    added test cases for OTP Service Impl

--- src/test/java/com/schoolmanagement/schoolmanagement/service/OtpServiceImplTest.java
+++ src/test/java/com/schoolmanagement/schoolmanagement/service/OtpServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.schoolmanagement.schoolmanagement.service;
 
 import com.schoolmanagement.schoolmanagement.constant.StaticFieldsAndMethods;
+import com.schoolmanagement.schoolmanagement.entity.Erole;
+import com.schoolmanagement.schoolmanagement.entity.Role;
+import com.schoolmanagement.schoolmanagement.entity.User;
 import com.schoolmanagement.schoolmanagement.entity.UserOtp;
 import com.schoolmanagement.schoolmanagement.exception.ResourceNotFoundException;
 import com.schoolmanagement.schoolmanagement.repository.OtpRepository;
@@ -11,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +30,12 @@ class OtpServiceImplTest {
 
     @Autowired
     OtpService otpService;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    EmailService emailService;
 
     private UserOtp userOtp;
 
@@ -67,11 +78,11 @@ class OtpServiceImplTest {
     }
 
     @Test
-    void validateOtp_when_token_not_present() {
+    void validateOtp_when_token_is_invalid() {
         when(otpRepository.findByToken(token)).thenReturn(null);
-        when(otpRepository.save(userOtp)).thenReturn(userOtp);
 
-        assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, otp), "Token Not present");
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, otp));
+        assertEquals("Invalid Token", exception.getMessage());
     }
 
     @Test
@@ -79,19 +90,19 @@ class OtpServiceImplTest {
         userOtp = new UserOtp(1L, email, token, expiredTokenCreationDateTime, otp);
 
         when(otpRepository.findByToken(token)).thenReturn(userOtp);
-        when(otpRepository.save(userOtp)).thenReturn(userOtp);
 
-        assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, otp), "Token Expired");
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, otp));
+        assertEquals("Token Expired", exception.getMessage());
     }
 
     @Test
     void validateOtp_when_otp_is_invalid() {
         when(otpRepository.findByToken(token)).thenReturn(userOtp);
-        when(otpRepository.save(userOtp)).thenReturn(userOtp);
 
         String invalidOtp = String.valueOf(Integer.parseInt(otp) + 1);
 
-        assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, invalidOtp), "Invalid OTP");
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> otpService.validateOtp(token, invalidOtp));
+        assertEquals("Invalid OTP", exception.getMessage());
     }
 
     @Test
@@ -122,6 +133,26 @@ class OtpServiceImplTest {
     }
 
     @Test
-    void sendOtpViaMail() {
+    void sendOtpViaMail_when_success() throws Exception {
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(1, Erole.ROLE_ADMIN));
+        User user = new User(1L, "username", email, "password", roles);
+
+        when(userService.findByEmail(email)).thenReturn(user);
+        doNothing().when(emailService).sendSimpleMail(any(), any(), any());
+        when(otpRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
+
+        otpService.sendOtpViaMail(email, "Testing send OTP");
+
+        verify(emailService, times(1)).sendSimpleMail(any(), any(), any());
+        verify(otpRepository, times(1)).save(any());
+    }
+
+    @Test
+    void sendOtpViaMail_when_no_account_found_with_given_email() {
+        when(userService.findByEmail(email)).thenReturn(null);
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> otpService.sendOtpViaMail(email, "Testing send OTP"));
+        assertEquals("No account found with this email", exception.getMessage());
     }
 }
